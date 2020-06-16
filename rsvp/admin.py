f2069e0bbd6086a41cda439185dd2ca40a76c7dd
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-06-16 22:59:47 +0100

    Added new models to admin view

--- rsvp/admin.py
+++ rsvp/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import RSVP
+from .models import RSVP, HomeCarousel, VillaCarousel
 
 # Register your models here.
 admin.site.register(RSVP)
+admin.site.register(HomeCarousel)
+admin.site.register(VillaCarousel)
