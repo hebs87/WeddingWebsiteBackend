0c76fb1214b7b6348b293fc0b42a46fb354096fd
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-06-16 22:52:44 +0100

    Created HomeCarouselViewSet

--- rsvp/views.py
+++ rsvp/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render, redirect
 from rest_framework import viewsets
-from .models import RSVP
-from .serializers import RSVPSerializer
+from .models import RSVP, HomeCarousel
+from .serializers import RSVPSerializer, HomeCarouselSerializer
 
 
 # Create your views here.
 class RSVPViewSet(viewsets.ModelViewSet):
     queryset = RSVP.objects.all()
     serializer_class = RSVPSerializer
+
+
+class HomeCarouselViewSet(viewsets.ModelViewSet):
+    queryset = HomeCarousel.objects.all()
+    serializer_class = HomeCarouselSerializer
