a1ca8ce163111d7cd41d73c76fa56c2df566ccdb
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-06-16 22:51:51 +0100

    Created HomeCarouselSerializer

--- rsvp/serializers.py
+++ rsvp/serializers.py
@@ -1,13 +1,21 @@
 from rest_framework import serializers
-from .models import RSVP
+from .models import RSVP, HomeCarousel
 
 
 class RSVPSerializer(serializers.ModelSerializer):
     """
     Serializes the Detail model data and returns as JSON
     """
-
     class Meta:
         model = RSVP
-        fields = ('guest_name', 'attending', 'not_attending', 'favourite_song',
+        fields = ('id', 'guest_name', 'attending', 'not_attending', 'favourite_song',
                   'favourite_drink', 'dietary_requirements', 'additional_info')
+
+
+class HomeCarouselSerializer(serializers.ModelSerializer):
+    """
+    Serializes the HomeCarousel image url and returns as JSON
+    """
+    class Meta:
+        model = HomeCarousel
+        fields = ('id', 'image_url')
