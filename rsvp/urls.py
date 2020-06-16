de013f2b1c76b452d38bea6d47ea08cd8385f58f
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-06-16 23:00:53 +0100

    Created new urls for new models

--- rsvp/urls.py
+++ rsvp/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 router = routers.DefaultRouter()
 router.register('rsvp', views.RSVPViewSet)
+router.register('home-carousel', views.HomeCarouselViewSet)
+router.register('villa-carousel', views.VillaCarouselViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
