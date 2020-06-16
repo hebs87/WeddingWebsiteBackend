77950713881995a12562eb4923af6acf967b92be
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-06-16 22:43:50 +0100

    Added media route config to urls and settings.py

--- WeddingWebsiteBackend/urls.py
+++ WeddingWebsiteBackend/urls.py
@@ -13,10 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
+from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
     path('sitepanel/', admin.site.urls),
     path('reply/', include('rsvp.urls'))
 ]
+
+urlpatterns += static(
+    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+)
