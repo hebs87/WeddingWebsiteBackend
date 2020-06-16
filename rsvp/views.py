52593583b1f122f839a80a4cbe506c6869b4209a
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-06-16 22:22:06 +0100

    Created RSVPViewSet

--- rsvp/views.py
+++ rsvp/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from rest_framework import viewsets
+from .models import RSVP
+from .serializers import RSVPSerializer
+
 
 # Create your views here.
+class RSVPViewSet(viewsets.ModelViewSet):
+    queryset = RSVP.objects.all()
+    serializer_class = RSVPSerializer
