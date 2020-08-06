2052890f4496159977aae5394ccd00706990aeb6
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-08-06 20:04:43 +0100

    Allowed all origins to CORS whitelist

--- WeddingWebsiteBackend/settings.py
+++ WeddingWebsiteBackend/settings.py
@@ -58,11 +58,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ORIGIN_WHITELIST = [
-    "http://localhost:3000",
-    "http://localhost:5000",
-    "https://kimmyandsunny-wedding-website.herokuapp.com/"
-]
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'WeddingWebsiteBackend.urls'
 
