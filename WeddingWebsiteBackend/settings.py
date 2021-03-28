14040b712d4ba5d615ddb836b573105651a06b20
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2021-03-28 23:07:15 +0100

    Added db.sqlite3 to git and updated settings.py for AWS

--- WeddingWebsiteBackend/settings.py
+++ WeddingWebsiteBackend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import dj_database_url
+# import dj_database_url
 import environ
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -29,7 +29,10 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [env('HOSTNAME')]
+ALLOWED_HOSTS = [
+    env('HOSTNAME'),
+    env('HOST_IP')
+]
 
 # Application definition
 
@@ -86,17 +89,17 @@ WSGI_APPLICATION = 'WeddingWebsiteBackend.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 # Local DATABASE
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # LIVE DATABASE
-DATABASES = {
-    'default': dj_database_url.parse(env('DATABASE_URL'))
-}
+# DATABASES = {
+#     'default': dj_database_url.parse(env('DATABASE_URL'))
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
