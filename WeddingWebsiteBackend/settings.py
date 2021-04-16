6dce9c128076f1696d7d3d316e96443be98f175e
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2021-04-16 23:29:25 +0100

    Added DEFAULT_AUTO_FIELD setting in settings.py to remove model pk field warning with latest Django version

--- WeddingWebsiteBackend/settings.py
+++ WeddingWebsiteBackend/settings.py
@@ -163,3 +163,6 @@ MEDIAFILES_LOCATION = "media"
 DEFAULT_FILE_STORAGE = "custom_storages.MediaStorage"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+
+# Prevent id primary key error
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
